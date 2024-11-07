7bd740335e562fc5166e7a78d3c5718016d9d233
hakanor <hakanor99@gmail.com>
2024-11-07 13:24:04 +0300

    first commit

---
+++ Sources/DemoSDK.xcframework/ios-arm64_x86_64-simulator/DemoSDK.framework/Headers/DemoSDK.h
@@ -0,0 +1,18 @@
+//
+//  DemoSDK.h
+//  DemoSDK
+//
+//  Created by Hakan Or on 6.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoSDK.
+FOUNDATION_EXPORT double DemoSDKVersionNumber;
+
+//! Project version string for DemoSDK.
+FOUNDATION_EXPORT const unsigned char DemoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoSDK/PublicHeader.h>
+
+
